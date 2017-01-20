81b0329b2d76845b192ee83ec186b412e75f1b5c
Phil Merrell <philmerrell@boisestate.edu>
2017-01-19 19:06:38 -0700

    feat: Add more portfolio projects

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,3 @@
-// var doc = $(document),
-//     elem = $('.navbar');
-
-// elem.hide();    
-// doc.scroll(function() {
-//   if (doc.scrollTop() >= 50) {
-//     // user scrolled 50 pixels or more;
-//     // do stuff
-//     elem.slideDown('fast');
-//   } else {
-//     elem.slideUp('fast');
-//   }
-// });
-// $('body').scrollspy({ offset: '#featured-work' })
 
 var navOffset = $('#navbar').height() + 25;
 
--- app/scripts/project-modal.js
+++ app/scripts/project-modal.js
@@ -1,52 +1,70 @@
 $(document).ready(function() {
     $('#projectModal').on('show.bs.modal', function (event) {
-        console.log('what');
         var button = $(event.relatedTarget) // Button that triggered the modal
         var projectIndex = button.data('project-index') // Extract info from data-* attributes
 
         var project = projects[parseInt(projectIndex)];
-        console.log(project);
-        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
         var modal = $(this);
         modal.find('.modal-title').text(project.name);
         modal.find('.description').text(project.description);
-        // modal.find('.modal-body input').val(recipient)
+
+        var websiteButton = modal.find('#website-button');
+        var githubButton = modal.find('#github-button');
+
+        (project.urls.website) ? websiteButton.show() : websiteButton.hide();
+        (project.urls.github) ? githubButton.show() : githubButton.hide();
+        
+        
     });
 
     var projects = [
         {
             name: 'Mock Event Planner',
             description: 'Built a responsive web application toward the Udacity Senior Web Developer Nanodegree that allows users to establish a meetup event. The application makes use of the latest best practices in developing high conversion web forms, including new input type elements, compound form components, data validation and handling touch input.',
-            urls: [
-                { type: 'website', url: '' },
-                { type: 'github', url: '' }
-            ]
+            urls: {
+                website: 'https://philmerrell.github.io/meet-up-event-planner/',
+                github: 'https://github.com/philmerrell/meet-up-event-planner'
+            }
         },
         {
             name: 'Movie Trailers',
             description: 'Built a responsive web application toward the Udacity Senior Web Developer Nanodegree that allows users to establish a meetup event. The application makes use of the latest best practices in developing high conversion web forms, including new input type elements, compound form components, data validation and handling touch input.',
-            urls: [
-                { type: 'website', url: ''},
-                { type: 'github', url: '' }
-            ]
+            urls: {
+                website: '',
+                github: ''
+            }
         },
         {
             name: 'Boise State University: Major Finder',
             description: 'Major Finder utilizes a client side build system via NPM, Bower and Grunt. Angular Material Design was used for many of the design components, which lend themselves well to both a desktop and mobile web experience.',
-            urls: [
-                { type: 'website', url: 'https://majors.boisestate.edu'}
-            ]
-            
+            urls: {
+                website: '',
+                github: ''
+            }
         },
         {
             name: 'Boise State University: myBoiseState Mobile',
             description: 'MyBoiseState Mobile is built for students and employees at Boise State University. It is a hybrid mobile application built with Ionic and is available on both the iOS AppStore and Google Play and leverages device features such as push notifications. This app\'s primary purpos is to serve as a portal that ties data from many different sources across the university into a single, easy to use platform.',
-            urls: [
-                { type: 'website', url: '' },
-                { type: 'ios', url: '' },
-                { type: 'android', url: ' ' }
-            ]
+            urls: {
+                ios: '',
+                android: ''
+            }
+        },
+        {
+            name: 'Famous Menu Example',
+            description: 'This project is a concept for a mobile side menu. As the user slides the content area, menu items are animated in. It was built using the Famous JavaScript library and has collected 14 stars on Github.',
+            urls: {
+                website: '',
+                github: ''
+            }
+        },
+        {
+            name: 'Famous Menu Example',
+            description: 'This project is a concept for a mobile side menu. As the user slides the content area, menu items are animated in. It was built using the Famous JavaScript library and has collected 14 stars on Github.',
+            urls: {
+                website: '',
+                github: ''
+            }
         }
     ];
 });
